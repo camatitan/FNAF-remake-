1cb709933a1aae8f08cb7e062e6280292755acfc
SpringLuz <49623029@est.ort.edu.ar>
2026-01-11 02:07:48 -0300

    11/1
    added office buttons and a semi door

--- fnaf-remake/scripts/main.gd
+++ fnaf-remake/scripts/main.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 @onready var animatedFan = $animatedFan
+@onready var animatedDoorR = $doorR
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animatedFan.Play("fan")
-	pass # Replace with function body.
+	animatedFan.play("fan")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event.is_action_pressed("space"):
+		animatedDoorR.play("door")
+	else: animatedDoorR.stop()
